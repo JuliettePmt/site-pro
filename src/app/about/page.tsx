1a18e04bfc05703cc1822455dd45b2cec0db29e9
Juliette Parmentier <juliette.parmentier@unamur.be>
2026-03-14 12:11:26 +0100

    PhD subject

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,7 +29,9 @@ return (
 {t.intro[1]} {t.bio[0]} {t.bio[1]} {t.bio[2]} <span className="text-xl">{t.disciplines[0]}</span> {t.bio[3]} <span className="text-xl text-[hsl(var(--primary-sky))]">{t.faculties[0]}</span> 
  {t.bio[4]} {t.bio[5]} {t.bio[6]} <span className="text-xl">{t.disciplines[1]}</span> {t.bio[8]} <span className="text-xl text-[hsl(var(--primary-sky))]">{t.faculties[1]}</span>.
 </p>
-
+<Heading>
+{t.phd_subject}
+</Heading>
 <p className=" font-poppins text-sm w-full text-tertiary max-sm:text-lg mt-7">{t.phd_subject}
 </p>
 </FramerWrapper>
--- src/lib/translations_about.ts
+++ src/lib/translations_about.ts
@@ -8,6 +8,7 @@ export const translations_about = {
     bio: ["doctorante", "en co-tutelle entre le", "département de", "de l'", " &","la", "faculté  d'", "Informatique", "de l'"],
     faculties: ["Université libre de Bruxelles", "Université de Namur"],
     profs: ["Louis Wiart", "Bruno Dumas"],
+    intro_phd:"Ma thèse",
     phd_subject:"Ma thèse vise à analyser la façon dont, sur Internet, les activités de lecture sont traduites en statistiques, notes, graphiques ou métriques quantifiées, et à saisir les effets de cette « mise en nombre » sur le rapport que les lecteurs entretiennent aux pratiques littéraires.",
   },
   en: {
@@ -19,6 +20,7 @@ export const translations_about = {
     bio: ["a PhD candidate", "in a joint supervision between the", "department of", "of the", " &", "the", "faculty of", "Computer Science", "of the"],
     faculties: ["Université libre de Bruxelles", "University of Namur"],
     profs: ["Louis Wiart", "Bruno Dumas"],
+    intro_phd:"My PhD thesis",
     phd_subject:"My PhD thesis aims to analyze how, on the Internet, reading activities are translated into statistics, ratings, graphs, or quantified metrics, and to understand the effects of this quantification on the relationship readers develop with literary practices.",
   },
 }
